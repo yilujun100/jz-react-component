8d19987c7a88223f5b1da5882d8aa598bc7c5f5c
yilujun100 <yilujun100@qq.com>
2023-10-19 17:46:43 +0800

    fix: build error

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-const meta = {
+const meta: Meta<typeof Button> = {
   title: 'Example/Button',
   component: Button,
   parameters: {
@@ -15,8 +15,8 @@ const meta = {
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
-  },
-} satisfies Meta<typeof Button>;
+  }
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
